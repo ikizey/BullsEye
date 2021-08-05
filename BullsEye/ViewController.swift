6684db8514b39c4c18d25aeaf69e201b2468b111
ikizey <ikizey@icloud.com>
2021-08-05 22:42:41 +0300

    add level restart

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
+    var targetValue = 0
     var currentValue = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        
+            updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
     }
 
     @IBAction func showAlert() {
         let message = "The value of the slider is \(currentValue)"
+                    + "\n The target value is \(targetValue)"
         
         let alert = UIAlertController(title: "Hello World!",
                                       message: message,
@@ -29,6 +46,8 @@ class ViewController: UIViewController {
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
